58147c92dc0faebceed95d9be2221fb5215b4ea5
enrique <kike@vjspain.com>
2017-07-21 18:57:06 +0200

    Basic client added

--- clientBulletTime.py
+++ clientBulletTime.py
@@ -11,12 +11,13 @@ r = requests.post("http://10.42.0.255:8000/stop")
 
 
 
-for i in range (41):
+for i in range (26,27):
+	print 'Pidiendo foto: ' + str(i)
 	file_url = 'http://10.42.0.' + str(100 + i) + ':8000/get/10'
 
 	try:
 		file_name = wget.download(file_url)
-		file_name.save('/pictures/' + str(i) + '_picture.png')
+		file_name.save('/Finals/' + str(i) + '_picture.png')
 
 	except:
 		print "File not received"
