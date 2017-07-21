1688451a97f53ccc6081913995d8e0ce51adb321
enrique <kike@vjspain.com>
2017-07-21 18:40:14 +0200

    URL fixed

--- clientBulletTime.py
+++ clientBulletTime.py
@@ -11,11 +11,12 @@ r = requests.post("http://10.42.0.255:8000/stop")
 
 
 
-file_url = 'http://10.42.0.100/get/10'
+for i in range (41):
+	file_url = 'http://10.42.0.' + str(100 + i) + ':8000/get/10'
 
-try:
-	file_name = wget.download(file_url)
-	file_name.save(/'pictures/10_picture.png')
+	try:
+		file_name = wget.download(file_url)
+		file_name.save('/pictures/' + str(i) + '_picture.png')
 
-except:
-	print "File not received"
+	except:
+		print "File not received"
